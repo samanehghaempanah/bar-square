cf79137891779b275c310aa58c946ac44b8cd4de
APADENPLAST/GhaemPanah <ghaempanah.s@gmail.com>
2024-04-26 19:55:18 +0330

    changes in dx-style & entities & auth.service

---
+++ src/app/shared/definitions/models/entities.model.ts
@@ -0,0 +1,11 @@
+export class GoodModel {
+  'GoodId': number;
+  'Type': number;
+  'Category': number;
+  'Name': string;
+  'Brand': number;
+  'Quality': any;
+  'Packing': number;
+  'Weight': number;
+  'PercentageDrop': number;
+}
--- src/app/shared/services/auth.service.ts
+++ src/app/shared/services/auth.service.ts
@@ -8,7 +8,7 @@ export interface IUser {
 
 const defaultPath = '/';
 const defaultUser = {
-  email: 'sandra@example.com',
+  email: 'gh.s@gmail.com',
   avatarUrl: 'https://js.devexpress.com/Demos/WidgetsGallery/JSDemos/images/employees/06.png'
 };
 
